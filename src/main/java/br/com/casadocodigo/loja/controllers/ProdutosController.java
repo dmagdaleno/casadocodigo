4ee845d3e0e01de29f9445561d85616d8f32d6d7
Diego Magdaleno <diegommagdaleno@gmail.com>
2017-04-07 16:21:43 -0300

    view de cadastro de produto

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,23 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProdutosController {
+
+    @RequestMapping("/produtos/form")
+    public String form(){
+        return "produtos/form";
+    }
+
+    @RequestMapping("/produtos")
+    public String gravar(String titulo, String descricao, int paginas){
+        System.out.println(titulo);
+        System.out.println(descricao);
+        System.out.println(paginas);
+
+        return "ok";
+    }
+
+}
