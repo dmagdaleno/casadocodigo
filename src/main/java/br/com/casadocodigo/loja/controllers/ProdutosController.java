9ee2ad87c45d5553d3474c9e02901f9dcc06a3ec
dmagdaleno <diegommagdaleno@gmail.com>
2017-04-07 23:34:47 -0300

    Configuração do JPA

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,10 +1,17 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
+
 @Controller
 public class ProdutosController {
+	
+	@Autowired
+	private ProdutoDAO produtoDao;
 
     @RequestMapping("/produtos/form")
     public String form(){
@@ -12,12 +19,10 @@ public class ProdutosController {
     }
 
     @RequestMapping("/produtos")
-    public String gravar(String titulo, String descricao, int paginas){
-        System.out.println(titulo);
-        System.out.println(descricao);
-        System.out.println(paginas);
-
-        return "ok";
+    public String gravar(Produto produto){
+        System.out.println(produto);
+        produtoDao.gravar(produto);
+        return "produtos/ok";
     }
 
 }
