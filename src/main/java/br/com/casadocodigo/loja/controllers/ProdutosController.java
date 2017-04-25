e7c2d0efcfaa7e91a25e2e3151e72e1fda5f635e
dmagdaleno <diegommagdaleno@gmail.com>
2017-04-24 22:31:59 -0300

    CCCde produtos

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -3,6 +3,7 @@ package br.com.casadocodigo.loja.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
@@ -10,23 +11,31 @@ import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @Controller
+@RequestMapping("produtos")
 public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDao;
 
-    @RequestMapping("/produtos/form")
+    @RequestMapping("/form")
     public ModelAndView form(){
     	ModelAndView model = new ModelAndView("produtos/form");
     	model.addObject("tipos", TipoPreco.values());
         return model;
     }
 
-    @RequestMapping("/produtos")
+    @RequestMapping(method=RequestMethod.POST)
     public String gravar(Produto produto){
         System.out.println(produto);
         produtoDao.gravar(produto);
         return "produtos/ok";
+    }
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public ModelAndView listar(){
+    	ModelAndView modelAndView = new ModelAndView("produtos/lista");
+    	modelAndView.addObject("produtos", produtoDao.listar());
+    	return modelAndView;
     }
 
 }
