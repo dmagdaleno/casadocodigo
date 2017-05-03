2134a81fe7fca425e1d9a888b976532c3c91af05
dmagdaleno <diegommagdaleno@gmail.com>
2017-05-02 22:52:01 -0300

    começando a validação de formularios

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -1,7 +1,12 @@
 package br.com.casadocodigo.loja.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -17,6 +23,11 @@ public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDao;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.addValidators(new ProdutoValidation());
+	}
 
     @RequestMapping("/form")
     public ModelAndView form(){
@@ -26,8 +37,10 @@ public class ProdutosController {
     }
 
     @RequestMapping(method=RequestMethod.POST)
-    public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
-        System.out.println(produto);
+    public ModelAndView gravar(@Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes){
+    	if(result.hasErrors()){
+    		return form();
+    	}
         produtoDao.gravar(produto);
         redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso.");
         return new ModelAndView("redirect:produtos");
